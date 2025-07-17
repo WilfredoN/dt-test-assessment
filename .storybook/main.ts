917a72bfe2f7782974dceb950ddaa52517f4b409
WilfredoN <thekillermaps@gmail.com>
2025-07-17 17:08:36 +0300

    chore: scss install instead

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,14 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": ["@storybook/addon-styling-webpack"],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  },
+  viteFinal: async (config) => {
+    return config
   }
-};
-export default config;
+}
+export default config
